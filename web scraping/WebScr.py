9b2f4066e1ccea39d4813aa27b2e441a67c695db
Prince Joshi <129651719+PrinceJoshi312@users.noreply.github.com>
2024-09-11 14:34:58 +0530

    Update WebScr.py

--- web scraping/WebScr.py
+++ web scraping/WebScr.py
@@ -2,13 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+url = '' #put your url here to do web scraping
 
 req = requests.get(url)
 
 page = BeautifulSoup(req.text, features='html.parser')
 
-table = page.find_all('table')[0]
+table = page.find_all('table')[0] # specify the index of your table in my case it is 0
 titles = table.find_all('th')
 table_titles = [title.text.strip() for title in titles]
 
@@ -22,4 +22,4 @@ for row in column_data[1:]:
     length = len(df)
     df.loc[length] = individual_row_data
 
-df.to_csv(r'C:\Users\HP\OneDrive\Pictures\Companies.csv', index = False)
+df.to_csv(r'/Companies.csv', index = False) #put your path where you want to save the csv file also ''' your path here /Companies ''' is the name of the csv file
